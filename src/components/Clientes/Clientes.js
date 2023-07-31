07b7b1f7d638c433f3342309df49a72c2800e590
Marcelo López <marcelolopezhernandez@gmail.com>
2023-07-30 20:00:54 -0400

    Feature

--- src/components/Clientes/Clientes.js
+++ src/components/Clientes/Clientes.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./clientes.css";
 import javierbeltran from "../../assets/javierbeltran.png";
 import ebnervinibaldo from "../../assets/ebnervinibaldo.png";
 import anavesquez from "../../assets/anavasquez.png";
 import daisygarcia from "../../assets/daisygarcia.png";
+import carlostraipe from "../../assets/carlostraipe.png";
 
-const clientes = [
+let clientes = [
   {
     id: 1,
     nombre: "Ebner Concha Saavedra",
@@ -36,7 +37,13 @@ const clientes = [
       "Me impresionó la rapidez con la que cambiaron mis ventanas y el orden que mantienen cuando las instalan, además la calefacción del hogar mejoró bastante, un 7 Termisur.",
     img: daisygarcia,
   },
-  
+  {
+    id: 5,
+    nombre: "Carlos Traipe Bocca",
+    testimonio:
+      "Me agradó la forma de trabajar de Termisur, son personas agradables que inspiran confianza, el trabajo que realizaron en mi casa fue excelente, además me solucionaron alguno problemas con otras instalaciones que tenía anteriormente, muy buen trabajo el de Termisur.",
+    img: carlostraipe,
+  },
 ];
 
 export const Clientes = () => {
@@ -50,21 +57,33 @@ export const Clientes = () => {
     setExpandedTestimonio(null);
   };
 
+  const  shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  clientes = shuffleArray(clientes);
+
   return (
     <div id="clientes" className="container-fluid">
-      <div className="row">
-        <div className="col-md-12 text-center">
-          <h2 data-aos="fade-up">Nuestros Clientes</h2>
-          <p data-aos="fade-up">
-            Nuestros clientes satisfechos son la prueba de nuestro excelente
-            trabajo. Sus testimonios reflejan el nivel de calidad y servicio que
-            ofrecemos en Termisur.
-          </p>
-        </div>
-        <div>
-          <Row xs={1} md={4} className="g-4">
+      <Container>
+        <Row>
+          <Col md={12} className="text-center">
+            <h2 data-aos="fade-up">Nuestros Clientes</h2>
+            <p data-aos="fade-up">
+              Nuestros clientes satisfechos son la prueba de nuestro excelente
+              trabajo. Sus testimonios reflejan el nivel de calidad y servicio
+              que ofrecemos en Termisur.
+            </p>
+          </Col>
+        </Row>
+        <div className="scrolling-container">
+          <div className="testimonios-row">
             {clientes.map((cliente) => (
-              <Col key={cliente.id}>
+              <div key={cliente.id} className="card-wrapper">
                 <Card
                   onMouseEnter={() => handleMouseEnter(cliente.id)}
                   onMouseLeave={handleMouseLeave}
@@ -75,19 +94,17 @@ export const Clientes = () => {
                   <Card.Img src={cliente.img} />
                   <Card.Body>
                     <Card.Title>{cliente.nombre}</Card.Title>
-                    <hr className="hr"></hr>
+                    <hr className="hr" />
                     <p className="testimonio" title={cliente.testimonio}>
-                      {expandedTestimonio === cliente.id
-                        ? cliente.testimonio
-                        : cliente.testimonio}
+                      {cliente.testimonio}
                     </p>
                   </Card.Body>
                 </Card>
-              </Col>
+              </div>
             ))}
-          </Row>
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
